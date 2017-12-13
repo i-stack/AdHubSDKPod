360d9e7ea5225c61224ef7ab423cd4efd6eb0354
stack <songshoubing7664@163.com>
2017-12-13 16:04:05 +0800

    fix native

--- AdHubSDKPod/Classes/AdHubSDK.framework/Versions/A/Headers/AdHubNativeAdDataModel.h
+++ AdHubSDKPod/Classes/AdHubSDK.framework/Versions/A/Headers/AdHubNativeAdDataModel.h
@@ -25,8 +25,9 @@ typedef NS_ENUM(NSUInteger){
 @property (nonatomic,readonly,strong)NSString *imageUrlString; // Deprecate use `images` replace
 @property (nonatomic,readonly,strong)NSString *body; // Deprecate use `texts` replace
 @property (nonatomic,readonly,strong)NSString *action;
-@property (nonatomic,readonly,strong)NSString *landingUrl; // landing page url
-@property (nonatomic,readonly,strong)NSString *deeplinkUrl;// deeplink
+@property (nonatomic,readonly,strong)NSString *landingUrl;  // landing page url
+@property (nonatomic,readonly,strong)NSString *deeplinkUrl; // deeplink
+@property (nonatomic,readonly,strong)NSString *phoneNumber; // phoneNumber
 @property (nonatomic,readonly,strong)NSArray *images; // @[imageUrlString, ...]
 @property (nonatomic,readonly,strong)NSArray *videos; // @[videoUrlString, ...]
 @property (nonatomic,readonly,strong)NSArray *texts;  // @[body, ...]
